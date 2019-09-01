7417a61885840dc362c8bd180d76532febbe7f55
Stark-JC <yejiacheng_stark@126.com>
2019-09-01 21:06:36 +0800

    init

---
+++ util/BPE.py
@@ -0,0 +1,41 @@
+import re
+import sys
+import collections
+
+
+def get_stats(vocab):
+    pairs = collections.defaultdict(int)
+    for word, freq in vocab.items():
+        symbols = word.split()
+        for i in range(len(symbols) - 1):
+            pairs[symbols[i], symbols[i + 1]] += freq
+    return pairs
+
+
+def merge_vocab(pair, v_in):
+    v_out = {}
+    bigram_pattern = re.escape(' '.join(pair))
+    p = re.compile(r'(?<!\S)' + bigram_pattern + r'(?!\S)')
+    for word in v_in:
+        w_out = p.sub(''.join(pair), word)
+        v_out[w_out] = v_in[word]
+    return v_out
+
+
+vocab = {'l o w </w>': 5, 'l o w e r </w>': 2,
+         'n e w e s t </w>': 6, 'w i d e s t </w>': 3}
+final_vocab = set(' '.join(vocab.keys()).split())
+num_merges = 10
+for i in range(num_merges):
+    pairs = get_stats(vocab)
+    try:
+        best = max(pairs, key=pairs.get)
+        final_vocab.add(''.join(best))
+    except ValueError:
+        break
+    if pairs[best] < 2:
+        sys.stderr.write('no pair has frequency > 1. Stopping\n')
+        break
+    vocab = merge_vocab(best, vocab)
+    print(best)
+print(final_vocab)
